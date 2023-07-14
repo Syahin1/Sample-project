500da33bdfa61b593235d5165e9d296445a31fc5
Syahin1 <shaiksyahin11@gmail.com>
2023-07-14 19:11:05 +0530

    ok

---
+++ frontend/src/1.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+function App1(){
+    return(
+        <>
+    <h1><center><u>RESUME</u></center></h1>
+    <img src="favicon.ico" alt="Syahin" align="right"></img>
+    <h2><b>SHAIK SYAHIN</b></h2>
+    
+    <p><b>Email Id:</b>shaiksyahin11@gmail.com</p>
+    <p><b>Mobile No:</b>7569232676</p>
+    <p><b>Address:</b>H.no:4-90-1,parchur,bapatla(dt),523169</p>
+    <br></br>
+    
+    <br></br>
+    <hr></hr>
+    <h2><b><u>CAREER OBJECTIVE</u></b></h2>
+    <p><ul><li>Obtain a responsible position in an organisation with the best capabilities,skills and knowledge</li></ul></p>
+    <br></br>
+    <h2><b><u>ACADEMIC QUALIFICATIONS</u></b></h2>
+    <table>
+      <tr>
+        <th>Qualifications</th>
+        <th>Name of the institutions</th>
+        <th>Academic year</th>
+      </tr>
+      <tr>
+        <td>B.tech</td>
+        <td>SRKR Engineering College</td>
+        <td>2021-2025</td>
+      </tr>
+      <tr>
+        <td>Intermediate</td>
+        <td>Srichaitanya Junior College</td>
+        <td>2019-2021</td>
+      </tr>
+      <tr>
+        <td>10th</td>
+        <td>SFS  School</td>
+        <td>2018-2019</td>
+      </tr>
+    </table>
+    <h2><u>PROFESSIONAL SKILLS</u></h2>
+    <ul>
+      <li>c</li>
+      <li>python</li>
+      <li>java</li>
+      <li>c</li>
+      <li>webdevelopment</li>
+    </ul>
+    
+    <h2><u>PERSONAL DETAILS</u></h2>
+    <p><b>Father's name:</b>SK.Khajavali</p>
+    <p><b>Date of Birth:</b>12 September 2003</p>
+    <p><b>Gender:</b>Female</p>
+    <p><b>Nationality:</b>Indian</p>
+    <p><b>Languages known:</b>Telugu,Hindi,English</p>
+    <p><b>Hobbies:</b>listening music,watching movies,readingbooks</p>
+    </>
+    )
+
+}
+export default App1;
